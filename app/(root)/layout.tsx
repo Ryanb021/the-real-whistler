6afe37e6873bc07420114c3de52e98b7810d2536
Ryan Bagan <darcknight1980@gmail.com>
2023-09-14 00:09:51 -0700

    rightsidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <Topbar />
 
-          <main>
+          <main className="flex felx-row">
             <LeftSidebar />
 
             <section className="main-container">
