2226d9fb0f926945cbfe01d00362de5bac0ad33e
Ryan Bagan <darcknight1980@gmail.com>
2023-09-16 14:22:59 -0700

    backend update

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import Image from 'next/image';
 import { ChangeEvent, useState } from 'react';
 import { isBase64Image } from '@/lib/utils';
 import { useUploadThing } from '@/lib/uploadthing';
+import { updateUser } from '@/lib/actions/user.actions';
+import { usePathname, useRouter } from 'next/navigation';
 
 interface Props {
   user: {
@@ -37,6 +39,8 @@ const AccountProfile = ({ user, btnTitle }:
   Props) => {
   const [files, setFiles] = useState<File[]>([])
   const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -83,7 +87,14 @@ const AccountProfile = ({ user, btnTitle }:
       }
     }
 
-    // TODO: Update user profile
+    await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname
+  });
   }
 
   return (
