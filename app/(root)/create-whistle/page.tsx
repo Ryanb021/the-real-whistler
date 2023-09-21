e4f54e116e82b6e27727f5fffcf702ef2614be2c
Ryan Bagan <darcknight1980@gmail.com>
2023-09-20 23:47:28 -0700

    added post whistle functions and file

---
+++ app/(root)/create-whistle/page.tsx
@@ -0,0 +1,25 @@
+import PostWhistle from "@/components/forms/PostWhistle";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+
+async function Page() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect('/onboarding');
+
+  return (
+    <>
+      <h1 className="head-text">Create Whistle</h1>
+
+      <PostWhistle userId={userInfo.id} />
+    </>
+  )
+}
+
+export default Page;
---
+++ components/forms/PostWhistle.tsx
@@ -0,0 +1,5 @@
+function PostWhistle({ userId }: { userId: string}) {
+  return <h1> Post Whistle Form</h1>
+}
+
+export default PostWhistle;
