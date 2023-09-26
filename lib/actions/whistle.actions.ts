652c5986168ced0d5d953e1c0a01d407b08227bb
Ryan Bagan <darcknight1980@gmail.com>
2023-09-25 23:43:00 -0700

    pause for now

--- lib/actions/whistle.actions.ts
+++ lib/actions/whistle.actions.ts
@@ -32,3 +32,32 @@ export async function createWhistle({ text, author, communityId, path }: Params)
     throw new Error(`Failed to create whistle: ${error.message}`)
   }
 };
+
+export async function fetchPosts(pageNumber = 1, pageSize =20) {
+  connectToDB();
+
+  // calculate number of posts to skip
+  const skipAmount = (pageNumber -1) * pageSize;
+
+  const postsQuery = Whistle.find({ parentId: { $in: [null, undefined] } })
+  .sort({ createdAt: 'desc'})
+  .skip(skipAmount)
+  .limit(pageSize)
+  .populate({ path: 'author' })
+  .populate({
+    path: 'children',
+    populate: {
+      path: 'author',
+      model: User,
+      select: "_id name parentId image"
+    }
+  })
+
+  const totalPostsCount = await Whistle.countDocuments({ parentId: { $in: [null, undefined] } })
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext }
+}
