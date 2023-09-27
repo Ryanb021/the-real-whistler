7cc85d6f676b8e7ccab94d0f62b427b650a2dc78
Ryan Bagan <darcknight1980@gmail.com>
2023-09-27 12:52:06 -0700

    pause for now

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
+"use client"
+
 import { fetchPosts } from "@/lib/actions/whistle.actions";
 
 export default async function Home() {
 
   const result = await fetchPosts(1, 30);
+
+  console.log(result);
+
   return (
     <div>
       <h1 className="head-text text-left">Home</h1>
