502c5b12e80cc608010aaacd7e28d4e3f6c81e6c
Ryan Bagan <darcknight1980@gmail.com>
2023-09-27 23:21:03 -0700

    image on whistle post

--- components/cards/WhistleCard.tsx
+++ components/cards/WhistleCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import { string } from "zod"
 
 interface Props {
@@ -35,7 +37,23 @@ const WhistleCard = ({
   comments
 }: Props) => {
   return (
-    <article>
+    <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+      <div className="flex items-start justify-between">
+        <div className="flex w-full flex-1 flex-row gap-4">
+          <div className="flex flex-col items-center">
+            <Link href={`/profile/${author.id}`} className="relative h-1 w-11">
+              {/* <Image
+                src={author.image}
+                alt="Profile Image"
+                fill
+                className="cursor-pointer rounded-full"
+              /> */}
+            </Link>
+          </div>
+        </div>
+      </div>
+
+
       <h2 className="text-small-regular text-light-2">
         {content}
       </h2>
